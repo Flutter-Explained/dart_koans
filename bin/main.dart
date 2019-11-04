1c51e64291b60ccb2981dca35456c4af6eee9c54
myracledesign <weber.max.1988@live.de>
2019-11-04 21:42:48 +1300

    init project

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_koans/dart_koans.dart' as dart_koans;
+
+main(List<String> arguments) {
+  print('Hello world: ${dart_koans.calculate()}!');
+}
---
+++ test/dart_koans_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_koans/dart_koans.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
